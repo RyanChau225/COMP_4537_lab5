03573d082b4c31505e2b6f9979fc88220e38a79b
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-10-12 20:34:13 -0700

    update query input

--- client/public/client.js
+++ client/public/client.js
@@ -1,31 +1,47 @@
-document.getElementById("insertButton").addEventListener("click", insertData);
-document.getElementById("queryButton").addEventListener("click", executeQuery);
+document.addEventListener("DOMContentLoaded", function () {
+    const insertButton = document.getElementById("insertButton");
+    const executeButton = document.getElementById("executeButton");
+    const queryInput = document.getElementById("queryInput");
+    const responseDiv = document.getElementById("response");
 
-function insertData() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/insert', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("insertResponse").textContent = xhr.responseText;
-        }
-    };
-    xhr.send(JSON.stringify({}));
-}
+    // Function to make an HTTP request
+    function sendRequest(method, url, data, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                callback(null, JSON.parse(xhr.responseText));
+            } else {
+                callback(new Error("Request failed"));
+            }
+        };
+        xhr.send(JSON.stringify(data));
+    }
 
-function executeQuery() {
-    const query = document.getElementById("query").value;
-    const method = query.trim().toUpperCase().startsWith('SELECT') ? 'GET' : 'POST';
+    // Insert rows into the database
+    insertButton.addEventListener("click", function () {
+        sendRequest("POST", "http://Server2/api/v1/sql/insert", {}, function (error, response) {
+            if (error) {
+                responseDiv.innerHTML = "Error: " + error.message;
+            } else {
+                responseDiv.innerHTML = "Rows inserted: " + response.insertedRows;
+            }
+        });
+    });
 
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, '/query', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("queryResponse").textContent = xhr.responseText;
-        }
-    };
-    xhr.send(JSON.stringify({
-        query: query
-    }));
-}
+    // Execute SQL query
+    executeButton.addEventListener("click", function () {
+        const sqlQuery = queryInput.value;
+        const method = sqlQuery.trim().toLowerCase().startsWith("select") ? "GET" : "POST";
+        const url = "http://Server2/api/v1/sql/" + encodeURIComponent(sqlQuery);
+
+        sendRequest(method, url, {}, function (error, response) {
+            if (error) {
+                responseDiv.innerHTML = "Error: " + error.message;
+            } else {
+                responseDiv.innerHTML = JSON.stringify(response, null, 2);
+            }
+        });
+    });
+});
